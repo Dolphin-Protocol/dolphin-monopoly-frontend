573351b9bfa588c7cb2c4fc48cef14863802fa0d
cjmario89 <mario89cheng@gmail.com>
2025-04-27 09:57:03 +0800

    fix: emit gameState event after changeTurn event

--- contexts/GameContext.tsx
+++ contexts/GameContext.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import React, {
-	createContext,
-	useContext,
-	useEffect,
-	useState,
-	ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
 } from "react";
 import { useCustomWallet } from "./WalletContext";
 import { useSocket } from "./SocketContext";
@@ -13,86 +13,86 @@ import { ApiRoomData } from "../types/socket";
 import { useParams } from "next/navigation";
 
 interface GameContextType {
-	turnAddress: string | null;
-	isTurn: boolean;
-	roomData: ApiRoomData | null;
+  turnAddress: string | null;
+  isTurn: boolean;
+  roomData: ApiRoomData | null;
 }
 
 const GameContext = createContext<GameContextType | undefined>(undefined);
 
 export const useGame = () => {
-	const context = useContext(GameContext);
-	if (!context) {
-		throw new Error("useGame must be used within a GameProvider");
-	}
-	return context;
+  const context = useContext(GameContext);
+  if (!context) {
+    throw new Error("useGame must be used within a GameProvider");
+  }
+  return context;
 };
 
 export const useGameContext = useGame;
 
 interface GameProviderProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 export const GameProvider: React.FC<GameProviderProps> = ({ children }) => {
-	const { socket } = useSocket();
-	const { address } = useCustomWallet();
+  const { socket } = useSocket();
+  const { address } = useCustomWallet();
 
-	const [turnAddress, setTurnAddress] = useState<string | null>(null);
-	const [isTurn, setIsTurn] = useState<boolean>(false);
-	const [roomData, setRoomData] = useState<ApiRoomData | null>(null);
+  const [turnAddress, setTurnAddress] = useState<string | null>(null);
+  const [isTurn, setIsTurn] = useState<boolean>(false);
+  const [roomData, setRoomData] = useState<ApiRoomData | null>(null);
 
-	const { roomId } = useParams<{ roomId: string }>();
+  const { roomId } = useParams<{ roomId: string }>();
 
-	useEffect(() => {
-		if (!socket) return;
-		if (!roomId) return;
+  useEffect(() => {
+    if (!socket) return;
+    if (!roomId) return;
 
-		socket.emit("gameState", { roomId });
+    socket.emit("gameState", { roomId });
 
-		socket.on("ChangeTurn", (data) => {
-			console.log("ChangeTurn", data);
-			setTurnAddress(data.player);
-			setIsTurn(data.player === address);
-		});
-		socket.on("gameState", (data) => {
-			console.log("gameState", data);
-			if (!data) return;
-			setRoomData(data.gameState);
-		});
-		socket.on("Move", (data) => {
-			console.log("Move", data);
-		});
-		socket.on("ActionRequest", (data) => {
-			console.log("ActionRequest", data);
-		});
-		socket.on("Buy", (data) => {
-			console.log("Buy", data);
-		});
+    socket.on("ChangeTurn", (data) => {
+      console.log("ChangeTurn", data);
+      socket.emit("gameState", { roomId });
+      setTurnAddress(data.player);
+      setIsTurn(data.player === address);
+    });
 
-		socket.on("error", (data) => {
-			console.error("Socket error:", data.message);
-		});
+    socket.on("gameState", (data) => {
+      console.log("gameState", data);
+      if (!data) return;
+      setRoomData(data.gameState);
+    });
+    socket.on("Move", (data) => {
+      console.log("Move", data);
+    });
+    socket.on("ActionRequest", (data) => {
+      console.log("ActionRequest", data);
+    });
+    socket.on("Buy", (data) => {
+      console.log("Buy", data);
+    });
 
-		return () => {
-			socket.off("ChangeTurn");
-			socket.off("error");
-			socket.off("gameState");
-			socket.off("Move");
-			socket.off("ActionRequest");
-			socket.off("Buy");
-		};
-	}, [address, socket, roomId]);
+    socket.on("error", (data) => {
+      console.error("Socket error:", data.message);
+    });
 
-	const value = {
-		turnAddress,
-		isTurn,
-		roomData,
-	};
+    return () => {
+      socket.off("ChangeTurn");
+      socket.off("error");
+      socket.off("gameState");
+      socket.off("Move");
+      socket.off("ActionRequest");
+      socket.off("Buy");
+    };
+  }, [address, socket, roomId]);
 
-	return (
-		<GameContext.Provider value={value}>{children}</GameContext.Provider>
-	);
+  const value = {
+    turnAddress,
+    isTurn,
+    roomData,
+  };
+
+  return <GameContext.Provider value={value}>{children}</GameContext.Provider>;
 };
 
 export default GameContext;
